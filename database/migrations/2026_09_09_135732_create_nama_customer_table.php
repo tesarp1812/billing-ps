32cb4004a889a355cd9e544d9c7e38d0a2cdb4ea
helmi <137972846+teathricalhood122@users.noreply.github.com>
2026-09-10 15:58:39 +0700

    feat: update table nama_customer

--- database/migrations/2026_09_09_135732_create_nama_customer_table.php
+++ database/migrations/2026_09_09_135732_create_nama_customer_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('gender');
             $table->string('customer');
             $table->Integer('no_hp');
+            $table->text('alamat')->nullable();
             $table->timestamps();
         });
     }
